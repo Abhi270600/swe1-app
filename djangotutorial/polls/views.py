80d65fe39be2dfeed93dd57fadf8f0b3a78088bc
abhishek <abhishek.adinarayan@gmail.com>
2025-10-19 00:44:03 -0400

    Black format corrections

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
--- djangotutorial/tests/test_smoke.py
+++ djangotutorial/tests/test_smoke.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+
 class SmokeTest(TestCase):
     def test_truth(self):
         assert 1 + 1 == 2
